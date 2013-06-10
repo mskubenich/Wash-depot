6318655d92198108443b9015f011bd3bf1eba76c
MrTime <kfedia@gmail.com>
2013-06-10 15:50:07 +0300

    Create user and attach devise

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WashDepot::Application.config.secret_token = '4403139fcaace2a941f9b12044e828c49df274119b7b356ec405011d83f596258c520d4cf68c6d31a66d858530216a1ff5986b3413a95f00b5250ce983404a2e'
