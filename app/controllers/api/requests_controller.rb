75c872d252f63de0884ffdd3c04de6f556cd2380
MrTime <kfedia@gmail.com>
2013-06-28 19:21:36 +0300

    Request creation (without images)

--- app/controllers/api/requests_controller.rb
+++ app/controllers/api/requests_controller.rb
@@ -4,7 +4,7 @@ class Api::RequestsController < ApplicationController
 
 	before_filter :only_admin_manager, :only => :index
 	before_filter :only_admin, :only => [:update, :delete]
-	before_filter :retrieve_params, :only => [:update]
+	before_filter :retrieve_params, :only => [:update, :create]
 	before_filter :get_request, :only => [:show, :update, :destroy]
 
 	def index
@@ -15,6 +15,15 @@ class Api::RequestsController < ApplicationController
 	end
 
 	def create
+		@request = Request.new @params_options
+
+		respond_to do |format|
+			if @request.save
+				format.json {}
+			else
+				format.json {render :json => @request.errors.to_json }
+			end
+		end
 	end
 
 	def update
@@ -45,6 +54,9 @@ class Api::RequestsController < ApplicationController
 	end
 
 	def retrieve_params
+		
+		# make available mass assignment
+
 		@params_options = {}
 
 		unless params["current_status"].blank?
@@ -59,6 +71,36 @@ class Api::RequestsController < ApplicationController
 		unless params["last_review"].blank?
 			last_review = params["last_review"].to_s
 			@params_options[:last_reviewed] = DateTime.strptime(last_review,'%s') 
+		end
+
+		unless params['creation_date'].blank? 
+			@params_options[:creation_date] = DateTime.strptime(params['creation_date'].to_s,'%s')
+		end
+
+		unless params['location'].blank?
+			@params_options[:location_id] = Location.where(:name => params['location']).first.id
+		end
+
+		# &&&
+		unless params['requested_by'].blank?
+			@params_options['requested_by'] = Location.where(:name => params['location']).first.id
+		end
+		
+		unless params['description'].blank?
+			@params_options[:description] = params['description']
+		end
+
+		unless params['importance'].blank?
+			@params_options[:importance] = params['importance']
+		end
+
+		unless params['problem_area'].blank?
+			@params_options[:problem_area_id] = ProblemArea.where(:name => params['problem_area']).first.id
+		end
+
+		unless params['identifier'].blank?
+			# Set request id
+			@params_options[:id] = params['identifier']
 		end
 	end
 
