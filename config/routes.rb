b0027b03eae4284f115d62ea8d49bd7a248e0361
mskubenich <misha.skubenich@gmail.com>
2013-07-01 16:07:21 +0300

    add pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 WashDepot::Application.routes.draw do
+  get "pages/index"
+
   namespace :api do
     devise_for :users do
       post 'sessions' => 'sessions#create', :as => 'login'
@@ -58,7 +60,7 @@ WashDepot::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'public#index.html'
+  root :to => 'pages#index.html'
 
   # See how all your routes lay out with "rake routes"
 
