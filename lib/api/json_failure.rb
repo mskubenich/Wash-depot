715fd351ea7692afe74d4682523abb33221172ba
mskubenich <misha.skubenich@gmail.com>
2013-07-02 19:57:20 +0300

    midify devise failture response

--- lib/api/json_failure.rb
+++ lib/api/json_failure.rb
@@ -3,17 +3,12 @@ class Api::JsonFailure < Devise::FailureApp
 	include ActionController::Rendering
 
 	def respond
-    if http_auth?
-      puts '-------------------------------------http'
-    else
-      puts '-------------------------------------json'
-    end
-	    #if request
-	    #  # it is n the best way, but ...
-	    #  # render habl view
-	     render 'app/views/api/errors/require_auth', :status => 401
-	    #else
-	    #  super
-	    #end
+	    if request_format == '*/*'
+	      # it is n the best way, but ...
+	      # render habl view
+	      render 'app/views/api/errors/require_auth', :status => 401
+	    else
+	      super
+	    end
  	end
 end
